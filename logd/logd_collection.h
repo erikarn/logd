a7265ba24f665b24efd585f9895e331cc03bd400
Adrian Chadd <adrian@freebsd.org>
2017-06-09 23:27:22 -0700

    Ok, for now, don't have the filter object "own" a logd_source. Instead just have it be something that we push the filter rules into for what to accept logging for.

--- logd/logd_collection.h
+++ logd/logd_collection.h
@@ -9,9 +9,16 @@ struct logd_filter;
 typedef	int logd_collection_filter_cb_t(struct logd_collection *, void *,
 	    struct logd_filter *, struct logd_msg *);
 
+struct logd_collection_entry {
+	struct logd_collection *parent;
+	struct logd_filter *filt;
+	TAILQ_ENTRY(logd_collection_entry) node;
+	TAILQ_HEAD(, logd_source) src;
+};
+
 struct logd_collection {
-	/* The list of logd_source instances */
-	TAILQ_HEAD(, logd_filter) logd_list;
+	/* collection entries */
+	TAILQ_HEAD(, logd_collection_entry) entries;
 
 	struct {
 		void *cbdata;
--- logd/logd_filter.h
+++ logd/logd_filter.h
@@ -1,17 +1,10 @@
 #ifndef	__LOGD_FILTER_H__
 #define	__LOGD_FILTER_H__
 
-struct logd_source;
 struct logd_msg;
 struct logd_filter;
 
 struct logd_filter {
-	/* The list of logd_source instances */
-	TAILQ_HEAD(, logd_source) source_list;
-
-	/* We're on a list.. */
-	TAILQ_ENTRY(logd_filter) node;
-
 	struct {
 		void *cbdata;
 	} owner_cb;
