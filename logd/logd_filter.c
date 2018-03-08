6f2825864112899e08d5f9efa4cd5521bee40462
Adrian Chadd <adrian@freebsd.org>
2018-03-08 12:19:57 -0800

    Add initial blank logd_filter support.
    This implements a very simple deny-all default template for filtering.
    Some basic filter module things will show up soon!

---
+++ logd/logd_filter.c
@@ -0,0 +1,81 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <libutil.h>
+#include <strings.h>
+#include <string.h>
+#include <err.h>
+
+#include <sys/queue.h>
+
+#include <event2/event.h>
+
+#include "config.h"
+
+#include "logd_util.h"
+#include "logd_buf.h"
+#include "logd_msg.h"
+#include "logd_source.h"
+#include "logd_filter.h"
+
+/*
+ * Always return false (don't match.)
+ */
+static int
+logd_filter_null_check(struct logd_filter *lf, void *arg,
+    struct logd_source *src, struct logd_source *dst,
+    struct logd_msg *m)
+{
+
+	return (0);
+}
+
+static int
+logd_filter_null_free(struct logd_filter *lf, void *arg)
+{
+
+	return (0);
+}
+
+/*
+ * Allocate a default logd filter.  This is a blank template
+ * who will always fail filter requests.
+ */
+struct logd_filter *
+logd_filter_create(struct event_base *eb)
+{
+	struct logd_filter *lf;
+
+	lf = calloc(1, sizeof(*lf));
+	if (lf == NULL) {
+		warn("%s: calloc", __func__);
+		return (NULL);
+	}
+	lf->eb = eb;
+	lf->child_cb.filter_check_cb = logd_filter_null_check;
+	lf->child_cb.filter_free_cb = logd_filter_null_free;
+	lf->child_cb.cbdata = NULL;
+
+	return (lf);
+}
+
+int
+logd_filter_free(struct logd_filter *lf)
+{
+
+	lf->child_cb.filter_free_cb(lf, lf->child_cb.cbdata);
+
+	free(lf);
+	return (0);
+}
+
+int
+logd_filter_check(struct logd_filter *lf, struct logd_source *src,
+    struct logd_source *dst, struct logd_msg *m)
+{
+
+	return (lf->child_cb.filter_check_cb(lf, lf->child_cb.cbdata,
+	    src, dst, m));
+}
--- logd/logd_filter.h
+++ logd/logd_filter.h
@@ -2,12 +2,31 @@
 #define	__LOGD_FILTER_H__
 
 struct logd_msg;
+struct logd_source;
 struct logd_filter;
 
+typedef int logd_filter_check_cb_t(struct logd_filter *, void *,
+	    struct logd_source *src, struct logd_source *dst,
+	    struct logd_msg *);
+typedef	int logd_filter_free_cb_t(struct logd_filter *, void *);
+
 struct logd_filter {
+	struct event_base *eb;
+
 	struct {
 		void *cbdata;
 	} owner_cb;
+
+	struct {
+		void *cbdata;
+		logd_filter_check_cb_t * filter_check_cb;
+		logd_filter_free_cb_t * filter_free_cb;
+	} child_cb;
 };
+
+extern	struct logd_filter * logd_filter_create(struct event_base *eb);
+extern	int logd_filter_free(struct logd_filter *);
+extern	int logd_filter_check(struct logd_filter *, struct logd_source *,
+	    struct logd_source *, struct logd_msg *);
 
 #endif	/* __LOGD_FILTER_H__ */
